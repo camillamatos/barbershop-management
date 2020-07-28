7c86ff7da40aa28d345e0f83732c3e00826b56a6
Camilla Matos <camillapozerm@gmail.com>
2020-07-27 22:49:42 -0300

    frontend/perfil

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import 'react-day-picker/lib/style.css';
 
 import { FiPower, FiClock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import {
   Container,
   Header,
@@ -142,7 +143,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
@@ -171,6 +174,7 @@ const Dashboard: React.FC = () => {
                 />
 
                 <strong>{nextAppointment.user.name}</strong>
+
                 <span>
                   <FiClock />
                   {nextAppointment.hourFormatted}
